f0f3b3209d1b430189de297068fe5d7bcf0df494
Diuliano <diulianosmarquesmendes@gmail.com>
2025-02-22 12:19:53 -0300

    codigo BancoTerminal concluido

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+
+ import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o numero da Conta: ");
+        int numero = scanner.nextInt();
+
+        scanner.nextLine();
+
+        System.out.println("Por favor, Digite o numero da Agencia:");
+        String agencia = scanner.nextLine();
+
+
+
+        System.out.println("Por favor, digite o Nome do Cliente:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por Favor, Digite  o Saldo inicial: ");
+        double saldo = scanner.nextDouble();
+
+        scanner.close();
+ 
+        String mensagem =  " Olá " + nomeCliente + ", Obrigado por criar uma conta em nosso banco, sua Agencia é " + agencia + ", conta "+ numero + " e seu saldo " + saldo + "  Já esta disponivel para saque. ";
+        
+        System.out.println(mensagem);
     }
 }
